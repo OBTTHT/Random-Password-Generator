4f63e3350a8ea4b573bf174e4717a204f912a75e
OBTTHT <95370738+OBTTHT@users.noreply.github.com>
2023-01-11 19:09:10 +0300

    Update passgenerator.py

--- passgenerator.py
+++ passgenerator.py
@@ -39,4 +39,4 @@ import random
 passlen = int(input("Şifrenin Uzunluğunu Girin: "))
 s = "abcçdefgğhıijklmnoöprsştuüvyz0123456789ABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZ!@#$%^é*()?[]½£='"
 p = "".join(random.sample(s,passlen ))
-print("Your Password: "p)
+print("Your Password: ", p)
